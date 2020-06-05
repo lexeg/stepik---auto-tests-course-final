5ca3a6f8131c31e5b326ee28dd4cc720a064546d
ubuntu-gitlab <klim2041@yandex.ru>
2020-06-05 08:06:28 +0500

    add product page object. Add test case for check adding product to basket

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,8 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    PRODUCT_IN_BASKET_MESSAGE = (By.CSS_SELECTOR, "#messages .alert-success:nth-child(1) strong")
+    PRODUCT_PRICE_IN_BASKET_MESSAGE = (By.CSS_SELECTOR, "#messages .alert-info strong")
---
+++ pages/product_page.py
@@ -0,0 +1,18 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+from selenium.webdriver.common.by import By
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        basket_button.click()
+
+    def should_be_product_in_basket(self):
+        message = self.browser.find_element(*ProductPageLocators.PRODUCT_IN_BASKET_MESSAGE)
+        text = message.text
+        assert text == "The shellcoder's handbook", f"Book name in basket not equal to choosen book. Should be 'The shellcoder's handbook', but was {text}"
+
+    def should_be_price_in_basket_equal_to_product_price(self):
+        message = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_IN_BASKET_MESSAGE)
+        text = message.text
+        assert text == "£9.99", f"Book price in basket not equal to choosen book price. Should be '£9.99', but was {text}"

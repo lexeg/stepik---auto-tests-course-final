dd4a24f01ea517cffef9e703e877d5d8a6f91e89
ubuntu-gitlab <klim2041@yandex.ru>
2020-06-12 17:07:30 +0500

    fix: add user authorization check

--- test_product_page.py
+++ test_product_page.py
@@ -14,6 +14,7 @@ class TestUserAddToBasketFromProductPage():
         page = LoginPage(browser, link)
         page.open()
         page.register_new_user(email, password)
+        page.should_be_authorized_user()
 
     def test_user_cant_see_success_message(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
